223960ac10318c0b4524e3e06f77962d582bce01
jungla-team <44529886+jungla-team@users.noreply.github.com>
2019-12-17 16:03:40 +0100

    Update resizepicon.py

--- resizepicon.py
+++ resizepicon.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python
 from PIL import Image
 import os, sys
-import shutil
-
-path = sys.argv[1]
-dirs = os.listdir(path)
 
 def splitfilename(filename):
-    sname = ""
-    sext = ""
-    i = filename.rfind(".")
-    if (i != 0):
-        n = len(filename)
-        j = n-i-1
-        sname = filename[0:i]
-        sext = filename[-j:]    
-    return sname, sext
+     sname = ""
+     sext = ""
+     i = filename.rfind(".")
+     if (i != 0):
+         n = len(filename)
+         j = n-i-1
+         sname = filename[0:i]
+         sext = filename[-j:]    
+     return sname, sext
 
 def resize():
     for item in dirs:
@@ -26,5 +22,16 @@ def resize():
                 f, e = os.path.splitext(path+item)
                 imResize = im.resize((220,132), Image.ANTIALIAS)
                 imResize.save(f+'.png', 'png')
-
-resize()
+				
+if len(sys.argv) == 2:
+    len_cadena = len(sys.argv[1])
+    cadena = sys.argv[1]
+    caracter = cadena[len_cadena-1]
+    if caracter == "/":
+        path = sys.argv[1]
+    else:
+        path = sys.argv[1] + '/'
+    dirs = os.listdir(path)
+    resize()
+else:
+    print "Se debe pasar como argumento la ruta de los picons, ejemplo de llamada correcta: python resizepicon.py /media/hdd/picon"
